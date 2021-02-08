788842491c9d8ed49afb530a92bbffc004c1257b
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-08 14:54:44 -0500

    removing logger

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker
     }
 );
 
-app.use(logger("dev"));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
