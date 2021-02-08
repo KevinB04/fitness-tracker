4619157256aef1856363877dc2511a0c6ada58e8
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-08 09:30:23 -0500

    creating schema for workout.js

--- models/workout.js
+++ models/workout.js
@@ -8,9 +8,35 @@ constWorkoutSchema = new Schema({
         default: Date.now
     },
     exercises: [
-
-    ]
-})
+        {
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter exercise name"
+            },
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter exercise type"
+            },
+            weight: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            duration: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            }
+        }
+    ] 
+});
 
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
